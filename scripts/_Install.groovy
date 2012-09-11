40babeb5397de6bfb6392b9b5d21ad04a5ab174c
Sandro Martini <sandro.martini@gmail.com>
2012-09-11 11:07:40 +0200

    add some links to akka docs, and let the plugin create scala source folders (but without enabling them for now)

--- scripts/_Install.groovy
+++ scripts/_Install.groovy
@@ -4,7 +4,5 @@
 // by Gant (such as 'baseDir' which points on project base dir). You can
 // use 'ant' to access a global instance of AntBuilder
 //
-// For example you can create directory under project tree:
-//
-//    ant.mkdir(dir:"${basedir}/grails-app/jobs")
-//
+Ant.property(environment: "env")
+Ant.mkdir(dir: "${basedir}/src/scala")
