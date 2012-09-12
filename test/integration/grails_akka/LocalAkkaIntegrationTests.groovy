da0d058044a2a4ce64e55d9a9da6030705e835a0
Sandro Martini <sandro.martini@gmail.com>
2012-09-12 17:57:35 +0200

    update integration tests like unit tests

--- test/integration/grails_akka/LocalAkkaIntegrationTests.groovy
+++ test/integration/grails_akka/LocalAkkaIntegrationTests.groovy
@@ -1,10 +1,22 @@
 package grails_akka
 
 import static org.junit.Assert.*
+
+import grails.test.mixin.*
+import grails.test.mixin.support.*
 import org.junit.*
 import org.junit.rules.*
 
 import akka.actor.*
+import akka.routing.*
+import akka.util.Duration
+import akka.testkit.*
+
+// import java.util.concurrent.TimeUnit
+
+import grails_akka.actor.*
+import grails_akka.command.*
+import grails_akka.message.*
 
 
 /**
@@ -14,15 +26,53 @@ import akka.actor.*
  */
 class LocalAkkaIntegrationTests
 {
+    // global actor system for this test class, to reuse it in all test methods here
+    ActorSystem system = null
+    ActorRef    actor  = null
+
+    // the supported mode to mock an actor is with something like this:
+    TestProbe probe = null
+    ActorRef  mock  = null
+
 
     @Before
-    void setUp() {
-        // Setup logic here
+    public void setUp()
+    {
+        // creates the local actor system
+        // system = ActorSystem.apply()
+        system = ActorSystem.create("LocalActorSystem")
+        println("Created Local Actor System instance:  $system")
+
+        // get a reference to the greetingActor
+        // actor  = TestActorRef.apply(new Props(GreetingActor.class), system)  // this is the supported way
+        actor = system.actorOf(new Props(GreetingActor.class), "greeting_actor")  // this is my way (not test-specific) ...
+        println("Get Actor Reference to GreetingActor: $actor")
+
+
+        // the supported mode to mock an actor is with something like this:
+        // mock = TestActorRef.apply(new AbstractFunction0() {
+        //     @Override
+        //     public Pi.Worker apply() {
+        //         return new Pi.Worker();
+        //     }
+        // }, system);
+        // and in any test method then use something like this:
+        // probe = TestProbe.apply(system)
+        // mock  = probe.ref()
+        // mock.tell("Hello", probe.ref())
     }
 
     @After
-    void tearDown() {
-        // Tear down logic here
+    public void tearDown()
+    {
+        // shutdown the local actor system
+        // if (actor != null) {
+        //     actor.stop()
+        // }
+        // if (system != null) {
+        //     println("Shutting down Local Actor System instance: $system")
+        //     system.shutdown()
+        // }
     }
 
     @Test(expected = InstantiationException.class)
@@ -61,26 +111,26 @@ class LocalAkkaIntegrationTests
         log.info "testLocalAkkaSystem_Greetings()"
 
         // creates the local actor system
-        ActorSystem system = ActorSystem.create("GreetingSystem")
-        println("Actor System instance is: $system")
+        // ActorSystem system = ActorSystem.create("GreetingSystem")  // commented, to reuse the actor system global in this test class
+        println("Actor System    instance is: $system")
         assertNotNull system
 
         // get a reference to our greeting actor
-        ActorRef greetingActor = system.actorOf(new Props(GreetingActor.class), "greeting_actor")
-        println("Actor Reference instance is: $greetingActor")
-        assertNotNull greetingActor
+        // ActorRef actor = system.actorOf(new Props(GreetingActor.class), "greeting_actor")  // commented, to reuse the actor reference global in this test class
+        println("Actor Reference instance is: $actor")
+        assertNotNull actor
 
         // send to the greeting actor a null message
-        greetingActor.tell(null);
-        assertNotNull greetingActor  // dummy
+        actor.tell(null);
+        assertNotNull actor  // dummy
 
         // send to the greeting actor a Greeting message
-        greetingActor.tell(new Greeting("Test Greeting"));
-        assertNotNull greetingActor  // dummy
+        actor.tell(new Greeting("Test Greeting"));
+        assertNotNull actor  // dummy
 
         // send to the greeting actor an unknown message
-        greetingActor.tell(new String("Test String"));
-        assertNotNull greetingActor  // dummy
+        actor.tell(new String("Test String"));
+        assertNotNull actor  // dummy
     }
 
     @Test
@@ -89,37 +139,86 @@ class LocalAkkaIntegrationTests
         log.info "testLocalAkkaSystem_OtherCommands()"
 
         // creates the local actor system
-        ActorSystem system = ActorSystem.create("GreetingSystem")
-        println("Actor System instance is: $system")
+        // ActorSystem system = ActorSystem.create("GreetingSystem")  // commented, to reuse the actor system global in this test class
+        println("Actor System    instance is: $system")
         assertNotNull system
 
         // get a reference to our greeting actor
-        ActorRef greetingActor = system.actorOf(new Props(GreetingActor.class), "greeting_actor")
-        println("Actor Reference instance is: $greetingActor")
-        assertNotNull greetingActor
+        // ActorRef actor = system.actorOf(new Props(GreetingActor.class), "greeting_actor")  // commented, to reuse the actor reference global in this test class
+        println("Actor Reference instance is: $actor")
+        assertNotNull actor
 
-        // send to the greeting actor a sample Command message
-        greetingActor.tell(new Command("execute"));
-        assertNotNull greetingActor  // dummy
+        // send to the greeting actor a sample generic message
+        actor.tell(new GenericMessage<String>("simple generic message with a String"));
+        assertNotNull actor  // dummy
+    }
+
+    @Test
+    void testLocalAkkaSystem_Wait()
+    {
+        log.info "testLocalAkkaSystem_Wait()"
+
+        println("Actor System    instance is: $system")
+        assertNotNull system
+        println("Actor Reference instance is: $actor")
+        assertNotNull actor
 
         // send to the greeting actor a Wait message
         long sleepTime = 2000 // msec
         long startSleep = System.currentTimeMillis();
-        greetingActor.tell(new Wait(sleepTime));
+        actor.tell(new Wait(sleepTime));
         long stopSleep = System.currentTimeMillis();
         long delta = stopSleep - startSleep
         // TODO: enable later and make it working ...
         // assertTrue delta >= sleepTime
-        assertNotNull greetingActor  // dummy
+        assertNotNull actor  // dummy
+    }
 
-        // send to the greeting actor a shutdown message
-        greetingActor.tell(new Shutdown());
-        assertNotNull greetingActor  // dummy
+    @Test
+    void testLocalAkkaSystem_Stop()
+    {
+        log.info "testLocalAkkaSystem_Stop()"
 
-        // send to the greeting actor a sample Command message
+        println("Actor System    instance is: $system")
+        assertNotNull system
+        println("Actor Reference instance is: $actor")
+        assertNotNull actor
+
+        // send to the greeting actor a stop message
+        actor.tell(new Stop());
+        assertNotNull actor  // dummy
+
+        // // send to the greeting actor a shutdown message
+        // actor.tell(new Shutdown());
+        // assertNotNull actor  // dummy
+
+        // send to the greeting actor an unknown message
+        actor.tell(new String("Test String"));
+        assertNotNull actor  // dummy
         // ok, but note that this actor (after the Shutdown message) will not get this message ...
-        greetingActor.tell(new Command("execute2"));
-        assertNotNull greetingActor  // dummy
+    }
+
+    @Test
+    void testLocalAkkaSystem_ActorCommand()
+    {
+        log.info "testLocalAkkaSystem_ActorCommand()"
+
+        println("Actor System    instance is: $system")
+        assertNotNull system
+        println("Actor Reference instance is: $actor")
+        assertNotNull actor
+
+        // wrap sending message to the actor inside a command
+        ActorCommand cmd = new ActorCommand(actor, new GenericMessage<String>("ActorCommand: simple generic message with a String"))
+        println("ActorCommand    instance is: $cmd")
+        cmd.execute()
+        assertNotNull cmd  // dummy
+
+        // wrap sending message to the actor inside a command
+        ActorCommand cmd2 = new ActorCommand(actor, new String("ActorCommand: Test String"))
+        println("ActorCommand    instance is: $cmd2")
+        cmd2.execute()
+        assertNotNull cmd2  // dummy
     }
 
 }
--- test/integration/grails_akka/RemoteAkkaIntegrationTests.groovy
+++ test/integration/grails_akka/RemoteAkkaIntegrationTests.groovy
@@ -1,8 +1,22 @@
 package grails_akka
 
 import static org.junit.Assert.*
+
+import grails.test.mixin.*
+import grails.test.mixin.support.*
 import org.junit.*
 import org.junit.rules.*
+
+import akka.actor.*
+import akka.routing.*
+import akka.util.Duration
+import akka.testkit.*
+
+// import java.util.concurrent.TimeUnit
+
+import grails_akka.actor.*
+import grails_akka.command.*
+import grails_akka.message.*
 
 
 /**
@@ -12,15 +26,53 @@ import org.junit.rules.*
  */
 class RemoteAkkaIntegrationTests
 {
+    // global actor system for this test class, to reuse it in all test methods here
+    ActorSystem system = null
+    ActorRef    actor  = null
+
+    // the supported mode to mock an actor is with something like this:
+    TestProbe probe = null
+    ActorRef  mock  = null
+
 
     @Before
-    void setUp() {
-        // Setup logic here
+    public void setUp()
+    {
+        // creates the local actor system
+        // system = ActorSystem.apply()
+        system = ActorSystem.create("LocalActorSystem")
+        println("Created Local Actor System instance:  $system")
+
+        // get a reference to the greetingActor
+        // actor  = TestActorRef.apply(new Props(GreetingActor.class), system)  // this is the supported way
+        actor = system.actorOf(new Props(GreetingActor.class), "greeting_actor")  // this is my way (not test-specific) ...
+        println("Get Actor Reference to GreetingActor: $actor")
+
+
+        // the supported mode to mock an actor is with something like this:
+        // mock = TestActorRef.apply(new AbstractFunction0() {
+        //     @Override
+        //     public Pi.Worker apply() {
+        //         return new Pi.Worker();
+        //     }
+        // }, system);
+        // and in any test method then use something like this:
+        // probe = TestProbe.apply(system)
+        // mock  = probe.ref()
+        // mock.tell("Hello", probe.ref())
     }
 
     @After
-    void tearDown() {
-        // Tear down logic here
+    public void tearDown()
+    {
+        // shutdown the local actor system
+        // if (actor != null) {
+        //     actor.stop()
+        // }
+        // if (system != null) {
+        //     println("Shutting down Local Actor System instance: $system")
+        //     system.shutdown()
+        // }
     }
 
     @Test(expected = InstantiationException.class)
