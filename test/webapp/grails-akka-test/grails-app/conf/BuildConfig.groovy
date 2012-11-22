0b9e95c914dd24f1530241859e27cc1ab6dd0947
Sandro Martini <sandro.martini@gmail.com>
2012-11-22 17:38:58 +0100

    update some plugin release

--- test/webapp/grails-akka-test/grails-app/conf/BuildConfig.groovy
+++ test/webapp/grails-akka-test/grails-app/conf/BuildConfig.groovy
@@ -57,8 +57,9 @@ grails.project.dependency.resolution = {
         // compile "org.grails.plugins:mongodb:1.0.0.GA"
 
         runtime ":hibernate:$grailsVersion"
-        runtime ":jquery:1.8.0"
-        runtime ":resources:1.1.6"
+        runtime ":jquery:1.8.3"
+        // runtime ":resources:1.1.6"
+        runtime ":resources:1.2.RC2"
 
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
