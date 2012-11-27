b50c0e45a7477b8ac001e77060954d062f80b3cb
Sandro Martini <sandro.martini@gmail.com>
2012-11-27 15:35:03 +0100

    updated to release plugin 2.1.0, just before publishing my plugin version 0.6

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -60,7 +60,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build(":release:2.0.4", ":rest-client-builder:1.0.2") {
+        build(":release:2.1.0") {
             export = false
         }
     }
