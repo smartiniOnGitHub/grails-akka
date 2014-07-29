acb8450e8931ad03f1c84d1d0ae89936bffe2970
Sandro Martini <sandro.martini@gmail.com>
2014-07-29 17:21:53 +0200

    update plugins to latest release

--- test/webapp/grails-akka-test24/grails-app/conf/BuildConfig.groovy
+++ test/webapp/grails-akka-test24/grails-app/conf/BuildConfig.groovy
@@ -69,9 +69,9 @@ grails.project.dependency.resolution = {
 
         compile ":scaffolding:2.1.2"
         compile ':cache:1.1.7'
-        compile ":asset-pipeline:1.8.11"
+        compile ":asset-pipeline:1.9.4"
 
-        runtime ":hibernate4:4.3.5.4" // or ":hibernate:3.6.10.16"
+        runtime ":hibernate4:4.3.5.4"
         runtime ":database-migration:1.4.0"
         runtime ":jquery:1.11.1"
 
