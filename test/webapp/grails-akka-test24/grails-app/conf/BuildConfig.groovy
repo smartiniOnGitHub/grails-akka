21d208b214381e7e5e88d68ec5f429e382446487
Sandro Martini <sandro.martini@gmail.com>
2014-08-04 14:56:31 +0200

    update test-webapp24 to Grails-2.4.3, even its wrapper-related stuff

--- test/webapp/grails-akka-test24/grails-app/conf/BuildConfig.groovy
+++ test/webapp/grails-akka-test24/grails-app/conf/BuildConfig.groovy
@@ -61,7 +61,7 @@ grails.project.dependency.resolution = {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
         // runtime 'mysql:mysql-connector-java:5.1.31'
         // runtime 'org.postgresql:postgresql:9.3-1102-jdbc41'
-        test "org.grails:grails-datastore-test-support:1.0-grails-2.4"
+        test "org.grails:grails-datastore-test-support:1.0.1-grails-2.4"
     }
 
     plugins {
@@ -69,7 +69,7 @@ grails.project.dependency.resolution = {
 
         compile ":scaffolding:2.1.2"
         compile ':cache:1.1.7'
-        compile ":asset-pipeline:1.9.4"
+        compile ":asset-pipeline:1.9.6"
 
         runtime ":hibernate4:4.3.5.4"
         runtime ":database-migration:1.4.0"
